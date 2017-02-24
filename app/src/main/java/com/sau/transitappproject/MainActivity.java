3ca5d40eebd0ad80d70fc72a12ca07b7de1849ad
Saurabh Joshi <saurabhsjoshi94@gmail.com>
2017-02-24 14:24:11 -0500

    Added Google Play Library (for Maps API)
    Updated to API v25

--- app/src/main/java/com/sau/transitappproject/MainActivity.java
+++ app/src/main/java/com/sau/transitappproject/MainActivity.java
@@ -1,13 +1,17 @@
 package com.sau.transitappproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.MapView;
 
 public class MainActivity extends AppCompatActivity {
 
+    MapView m;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        m = (MapView) findViewById(R.id.mapView);
     }
 }
