00e79173e3041e1a4d74e6c95f310894c0d117f5
Saurabh Joshi <saurabhsjoshi94@gmail.com>
2017-02-24 14:10:41 -0500

    Init

---
+++ app/src/main/java/com/sau/transitappproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sau.transitappproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
