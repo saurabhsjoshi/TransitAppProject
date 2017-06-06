48e75ecd144fecae20685729d6ae0ce2f40e457a
Saurabh Joshi <joshio@uwindsor.ca>
2017-06-05 23:40:19 -0400

    Implemented Permissions
    Signed-off-by: Saurabh Joshi <joshio@uwindsor.ca>

--- app/src/main/java/com/sau/transitappproject/MainActivity.java
+++ app/src/main/java/com/sau/transitappproject/MainActivity.java
@@ -1,16 +1,39 @@
 package com.sau.transitappproject;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void dispatchPermission(View v) {
+        MainActivityPermissionsDispatcher.startMapActivityWithCheck(MainActivity.this);
+    }
+
+    @NeedsPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+    public void startMapActivity() {
         //Directly start maps activity
         startActivity(new Intent(this, MapsActivity.class));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // NOTE: delegate the permission handling to generated method
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
     }
 }
